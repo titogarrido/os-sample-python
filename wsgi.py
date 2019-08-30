64d29de3df8c0f264a61c1d7ed67b0db0fa268a7
Tito Garrido Ogando <titogarrido@gmail.com>
2019-08-30 15:10:56 -0300

    fix var name

--- wsgi.py
+++ wsgi.py
@@ -1,13 +1,13 @@
 from flask import Flask, render_template
 import sys
 
-app = Flask(__name__)
+application = Flask(__name__)
 
-@app.route('/')
-@app.route('/<name>')
+@application.route('/')
+@application.route('/<name>')
 def hello(name=None):
     version = sys.version
     return render_template('hello.html', name=name, version=version)
 
 if __name__ == "__main__":
-    app.run()
+    application.run()
