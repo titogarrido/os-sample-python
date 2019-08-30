219363ae7b77bedccd3f2028d0b75b0bb5e69789
Tito Garrido Ogando <titogarrido@gmail.com>
2019-08-30 14:37:15 -0300

    added version

--- wsgi.py
+++ wsgi.py
@@ -1,11 +1,13 @@
 from flask import Flask, render_template
+import sys
 
 app = Flask(__name__)
 
 @app.route('/')
 @app.route('/<name>')
 def hello(name=None):
-    return render_template('hello.html', name=name)
+    version = sys.version
+    return render_template('hello.html', name=name, version=version)
 
 if __name__ == "__main__":
     app.run()
